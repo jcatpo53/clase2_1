d12402e081761567a416590bf8af479a57bf390d
jcatpo53 <jonathancatma@gmail.com>
2020-05-27 05:32:19 -0500

    primer ejercicio

---
+++ Ejemplo01.R
@@ -0,0 +1,55 @@
+# COmentario
+# En Rstudio la combinacion de teclas CTRL + SHIFT + N  apertura un nuevo editor
+
+# Limpiar memoria
+rm(list =ls())
+
+
+# seteo/configuro el directorio de trabajo
+setwd("C:/Users/AZCH/Desktop/Clase2R4DS")
+
+# verficar : obtener el wd (working drectory)
+getwd()
+
+# Listar el contenido del wd
+dir()
+
+help(dir)
+dir(pattern = "csv")
+dir(pattern = "R")
+
+#hola como estas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
